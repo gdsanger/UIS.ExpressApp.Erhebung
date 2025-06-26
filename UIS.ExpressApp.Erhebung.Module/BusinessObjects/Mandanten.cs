491cafe3473907be930f9ceafb0917c19058da7f
Christian Angermeier <Christian.Angermeier+isar@isartec.de>
2025-06-26 12:08:44 +0200

    SQL Config in Mandanten für UIS hinzugefügt

--- UIS.ExpressApp.Erhebung.Module/BusinessObjects/Mandanten.cs
+++ UIS.ExpressApp.Erhebung.Module/BusinessObjects/Mandanten.cs
@@ -54,13 +54,42 @@ namespace UIS.ExpressApp.Erhebung.Module.BusinessObjects
             get { return GetPropertyValue<string>("UisOnlineStudentenLink"); }
             set { SetPropertyValue<string>("UisOnlineStudentenLink", value); }
         }
+        public string MSSQLServer
+        {
+            get { return GetPropertyValue<string>("MSSQLServer"); }
+            set { SetPropertyValue<string>("MSSQLServer", value); }
+        }
+        public string Datenbank
+        {
+            get { return GetPropertyValue<string>("Datenbank"); }
+            set { SetPropertyValue<string>("Datenbank", value); }
+        }
+        public string Benutzername
+        {
+            get { return GetPropertyValue<string>("Benutzername"); }
+            set { SetPropertyValue<string>("Benutzername", value); }
+        }
+        public string Passwort
+        {
+            get { return GetPropertyValue<string>("Passwort"); }
+            set { SetPropertyValue<string>("Passwort", value); }
+        }
 
         [Association("Mandant-Erhebung")]
         public XPCollection<Erhebung> Erhebungen
         { get { return GetCollection<Erhebung>("Erhebungen"); } }
 
 
-       
+        [Browsable(false)]
+        public string ConnectionString
+        {
+            get
+            {
+                if (MSSQLServer != null && Benutzername != null && Passwort != null && Datenbank != null)
+                    return string.Format("Data Source = {0}; Initial Catalog = {1}; Persist Security Info = True; User ID = {2}; Password = {3}", MSSQLServer, Datenbank, Benutzername, Passwort);
+                return "";
+            }
+        }
 
     }
 }
